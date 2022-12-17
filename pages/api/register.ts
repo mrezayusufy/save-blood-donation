e35b4ca20911a998401a3e4c4e17192a2efa90af
MReza Yusufy <reyu443@gmail.com>
2022-12-17 11:50:39 +0430

    register error fixed

--- pages/api/register.ts
+++ pages/api/register.ts
@@ -4,21 +4,14 @@ export default async (req, res) => {
   if (req.method === "POST") {
     const body = req.body;
     try {
-      // find the role
-      const role = await axios.get("http://localhost:3333/api/roles/type/"+body.role).then((res) => res.data).catch((e) => e.message);
       // register new user
       const response = await axios.post(
-            "http://localhost:3333/api/auth/local/register",
-            {
-              username: body.phone,
+            "http://localhost:3333/api/auth/local/register", {
               phone: body.phone,
-              email: body.phone + "@save.af",
               fullname: body.fullname,
               password: body.password,
-              role: role.id,
-              userRole: role,
-            }
-          )
+              role: body.role,
+            })
           .then((response) => response.data)
           .catch((error) => {
             res.send({ message: error.message});
